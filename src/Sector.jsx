c67a6ca73536417afa52dadad7c95a5ac87351ac
Mwaleh muturi <alexmuturi@gmail.com>
2020-11-27 11:18:32 +0300

    add pages

---
+++ src/Sector.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+
+import "./styles.css";
+
+const SectorSection = () => (
+  <Container maxWidth="md">
+    <Typography variant="h6">
+      Sector
+    </Typography>
+    
+  </Container>
+);
+export default SectorSection;
